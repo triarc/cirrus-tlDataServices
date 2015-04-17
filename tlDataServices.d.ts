0892335ead44bab3e412b4a5f3a97707c3147082
Marco Bachmann <mb@triarc-labs.com>
2015-04-17 07:59:59 +0200

    Bumped version to 0.0.11

---
+++ tlDataServices.d.ts
@@ -0,0 +1,55 @@
+declare module Triarc.Data {
+    class DataRequest<TResponse> {
+        method: string;
+        url: string;
+        data: any;
+        managedType: string;
+        returnType: string;
+        sendData: boolean;
+        constructor(method: string, url: string, data: any, managedType: string, returnType: string, sendData: boolean);
+    }
+}
+declare module Triarc.Data {
+    class DataResponse<T> {
+        data: T;
+        status: RequestStatus;
+        httpReponse: ng.IHttpPromiseCallbackArg<T>;
+        constructor(data: T, status?: RequestStatus, httpReponse?: ng.IHttpPromiseCallbackArg<T>);
+        isSuccessful(): boolean;
+        isFailure(): RequestStatus;
+        isValidationSuccessful(): boolean;
+        isValidationFailure(): boolean;
+    }
+    enum RequestStatus {
+        Success = 0,
+        Failure = 1,
+        ValidationFailure = 2,
+        ValidationSuccess = 3,
+        Unknown = 4,
+    }
+}
+declare module Triarc.Data {
+    function appendUrlParameter(url: string, key: string, value: string): string;
+}
+declare module Triarc.Data {
+    function convertDateStringsToDates(input: any): any;
+    var mod: ng.IModule;
+    class RequestSenderService {
+        private urlByService;
+        private $http;
+        private $q;
+        private $location;
+        constructor(urlByService: {
+            [serviceName: string]: string;
+        }, $http: ng.IHttpService, $q: ng.IQService, $location: ng.ILocationService);
+        getUrl(serviceName: string): string;
+        requestValue<T>(dataRequest: DataRequest<T>): ng.IPromise<DataResponse<T>>;
+    }
+    class RequestSenderProvider implements ng.IServiceProvider {
+        setUrl(newUrl: string, proxyServiceName: string): void;
+        $get: (string | (($http: ng.IHttpService, $q: ng.IQService, $location: ng.ILocationService) => RequestSenderService))[];
+        urlPerService: {
+            [serviceName: string]: string;
+        };
+    }
+}
--- tlDataServices.js
+++ tlDataServices.js
@@ -1,1 +1,188 @@
+var Triarc;
+(function (Triarc) {
+    var Data;
+    (function (Data) {
+        var DataRequest = (function () {
+            function DataRequest(method, url, data, managedType, returnType, sendData) {
+                this.method = method;
+                this.url = url;
+                this.data = data;
+                this.managedType = managedType;
+                this.returnType = returnType;
+                this.sendData = sendData;
+            }
+            return DataRequest;
+        })();
+        Data.DataRequest = DataRequest;
+    })(Data = Triarc.Data || (Triarc.Data = {}));
+})(Triarc || (Triarc = {}));
+var Triarc;
+(function (Triarc) {
+    var Data;
+    (function (Data) {
+        var DataResponse = (function () {
+            function DataResponse(data, status, httpReponse) {
+                if (status === void 0) { status = 0 /* Success */; }
+                this.data = data;
+                this.status = status;
+                this.httpReponse = httpReponse;
+            }
+            DataResponse.prototype.isSuccessful = function () {
+                return this.status == 0 /* Success */;
+            };
+            DataResponse.prototype.isFailure = function () {
+                return this.status = 1 /* Failure */;
+            };
+            DataResponse.prototype.isValidationSuccessful = function () {
+                return this.status == 3 /* ValidationSuccess */;
+            };
+            DataResponse.prototype.isValidationFailure = function () {
+                return this.status == 2 /* ValidationFailure */;
+            };
+            return DataResponse;
+        })();
+        Data.DataResponse = DataResponse;
+        (function (RequestStatus) {
+            RequestStatus[RequestStatus["Success"] = 0] = "Success";
+            RequestStatus[RequestStatus["Failure"] = 1] = "Failure";
+            RequestStatus[RequestStatus["ValidationFailure"] = 2] = "ValidationFailure";
+            RequestStatus[RequestStatus["ValidationSuccess"] = 3] = "ValidationSuccess";
+            RequestStatus[RequestStatus["Unknown"] = 4] = "Unknown";
+        })(Data.RequestStatus || (Data.RequestStatus = {}));
+        var RequestStatus = Data.RequestStatus;
+    })(Data = Triarc.Data || (Triarc.Data = {}));
+})(Triarc || (Triarc = {}));
+var Triarc;
+(function (Triarc) {
+    var Data;
+    (function (Data) {
+        function appendUrlParameter(url, key, value) {
+            // best guess, just check the url to see if we already have a ? for parameters
+            if (url.indexOf("?") == -1) {
+                url = url + "?";
+            }
+            key = encodeURI(key);
+            value = encodeURI(value);
+            var kvp = url.split('&');
+            var i = kvp.length;
+            var x;
+            while (i--) {
+                x = kvp[i].split('=');
+                if (x[0] == key) {
+                    x[1] = value;
+                    kvp[i] = x.join('=');
+                    break;
+                }
+            }
+            if (i < 0) {
+                kvp[kvp.length] = [key, value].join('=');
+            }
+            //this will reload the page, it's likely better to store this until finished
+            return kvp.join('&');
+        }
+        Data.appendUrlParameter = appendUrlParameter;
+    })(Data = Triarc.Data || (Triarc.Data = {}));
+})(Triarc || (Triarc = {}));
+/// <reference path="datarequest.ts" />
+/// <reference path="dataresponse.ts" />
+/// <reference path="urlfunctions.ts" />
+var Triarc;
+(function (Triarc) {
+    var Data;
+    (function (Data) {
+        angular.module('ng').config([
+            "$httpProvider",
+            function ($httpProvider) {
+                $httpProvider.defaults.transformResponse.push(function (responseData) {
+                    convertDateStringsToDates(responseData);
+                    return responseData;
+                });
+            }
+        ]);
+        function convertDateStringsToDates(input) {
+            // Ignore things that aren't objects.
+            if (typeof input !== "object")
+                return input;
+            for (var key in input) {
+                if (!input.hasOwnProperty(key))
+                    continue;
+                var value = input[key];
+                // Check for string properties which look like dates.
+                // TODO: Improve this regex to better match ISO 8601 date strings.
+                if (typeof value === "string") {
+                    var parsedDate = Date.parse(value);
+                    if (!isNaN(parsedDate)) {
+                        input[key] = parsedDate;
+                    }
+                }
+                else if (typeof value === "object") {
+                    // Recurse into object
+                    convertDateStringsToDates(value);
+                }
+            }
+            return input;
+        }
+        Data.convertDateStringsToDates = convertDateStringsToDates;
+        Data.mod = angular.module("tlDataServices", []);
+        Data.mod.config([
+            '$httpProvider',
+            function ($httpProvider) {
+                $httpProvider.defaults.headers.post = {
+                    'Content-Type': 'application/json'
+                };
+            }
+        ]);
+        var RequestSenderService = (function () {
+            function RequestSenderService(urlByService, $http, $q, $location) {
+                this.urlByService = urlByService;
+                this.$http = $http;
+                this.$q = $q;
+                this.$location = $location;
+            }
+            RequestSenderService.prototype.getUrl = function (serviceName) {
+                var url = this.urlByService[serviceName];
+                if (angular.isString(url))
+                    return url;
+                return "api";
+            };
+            RequestSenderService.prototype.requestValue = function (dataRequest) {
+                var deferred = this.$q.defer();
+                // make the http call
+                this.$http({
+                    method: dataRequest.method,
+                    url: dataRequest.url,
+                    data: dataRequest.sendData ? dataRequest.data : null
+                }).then(function (response) {
+                    var responseData = response.data;
+                    if (dataRequest.returnType == "boolean")
+                        responseData = (responseData == "true");
+                    else if (dataRequest.returnType == "number")
+                        responseData = +responseData;
+                    else if (dataRequest.returnType != "string" && responseData == "null")
+                        responseData = null;
+                    deferred.resolve(new Data.DataResponse(responseData, 0 /* Success */, response));
+                }, function (response) {
+                    deferred.reject(new Data.DataResponse(response.data, 1 /* Failure */, response));
+                });
+                return deferred.promise;
+            };
+            return RequestSenderService;
+        })();
+        Data.RequestSenderService = RequestSenderService;
+        var RequestSenderProvider = (function () {
+            function RequestSenderProvider() {
+                var _this = this;
+                this.$get = ['$http', '$q', '$location', function ($http, $q, $location) {
+                    return new RequestSenderService(_this.urlPerService, $http, $q, $location);
+                }];
+            }
+            RequestSenderProvider.prototype.setUrl = function (newUrl, proxyServiceName) {
+                this.urlPerService[proxyServiceName] = newUrl;
+            };
+            return RequestSenderProvider;
+        })();
+        Data.RequestSenderProvider = RequestSenderProvider;
+        Data.mod.provider('$requestSender', [RequestSenderProvider]);
+    })(Data = Triarc.Data || (Triarc.Data = {}));
+})(Triarc || (Triarc = {}));
 
