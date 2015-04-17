8872374dd3840172110772f11d91686ba82a3956
Marco Bachmann <mb@triarc-labs.com>
2015-04-17 15:39:38 +0200

    Bumped version to 0.0.14

--- tlDataServices.js
+++ tlDataServices.js
@@ -99,6 +99,7 @@ var Triarc;
                 });
             }
         ]);
+        var regexIso8601 = /^(\d{4}|\+\d{6})(?:-(\d{2})(?:-(\d{2})(?:T(\d{2}):(\d{2}):(\d{2})\.(\d{1,})(Z|([\-+])(\d{2}):(\d{2}))?)?)?)?$/;
         function convertDateStringsToDates(input) {
             // Ignore things that aren't objects.
             if (typeof input !== "object")
@@ -107,10 +108,9 @@ var Triarc;
                 if (!input.hasOwnProperty(key))
                     continue;
                 var value = input[key];
-                // Check for string properties which look like dates.
-                // TODO: Improve this regex to better match ISO 8601 date strings.
-                if (typeof value === "string") {
-                    var parsedDate = Date.parse(value);
+                var match;
+                if (typeof value === "string" && (match = value.match(regexIso8601))) {
+                    var parsedDate = Date.parse(match[0]);
                     if (!isNaN(parsedDate)) {
                         input[key] = parsedDate;
                     }
